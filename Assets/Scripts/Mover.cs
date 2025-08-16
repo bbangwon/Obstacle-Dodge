3c70f9620ee5d67cc99b22a9915899704c31deea
BreadOne <bbangwon.chung@gmail.com>
2025-08-16 17:59:48 +0900

    Unity 6.2.0f 업데이트

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -6,13 +6,11 @@ public class Mover : MonoBehaviour
     [SerializeField] float yValue = 0f;
     [SerializeField] float zValue = 0f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
 
     }
 
-    // Update is called once per frame
     void Update()
     {
         transform.Translate(xValue, yValue, zValue);
