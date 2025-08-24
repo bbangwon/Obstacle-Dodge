70583b874a24bac0c0296462e925b9f9721f9a96
BreadOne <bbangwon.chung@gmail.com>
2025-08-24 14:01:26 +0900

    인풋시스템 이용. 캐릭터 움직이도록 수정

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,18 +1,57 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class Mover : MonoBehaviour
+public class Mover : MonoBehaviour, MyActions.IPlayerActions
 {
-    [SerializeField] float xValue = 0f;
-    [SerializeField] float yValue = 0f;
-    [SerializeField] float zValue = 0f;
 
-    void Start()
+    private MyActions m_Actions;
+    private MyActions.PlayerActions m_Player;
+    Vector2 inputVec2;
+
+
+    void Awake()
     {
+        m_Actions = new MyActions();
+        m_Player = m_Actions.Player;
+        m_Player.AddCallbacks(this);
+    }
 
+    void OnDestroy()
+    {
+        m_Actions.Dispose();
+    }
+
+    void OnEnable()
+    {
+        m_Player.Enable();
+    }
+
+    void OnDisable()
+    {
+        m_Player.Disable();
     }
 
     void Update()
     {
+        float xValue = inputVec2.x;
+        float yValue = 0f;
+        float zValue = inputVec2.y;
+
         transform.Translate(xValue, yValue, zValue);
+    }
+
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        inputVec2 = context.ReadValue<Vector2>();
     }
 }
