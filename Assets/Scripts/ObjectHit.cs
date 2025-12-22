f6bba78de14982856f1982e11538f408eedb0e96
BreadOne <bbangwon.chung@gmail.com>
2025-12-22 14:55:56 +0900

    GetComponent() 사용하기

--- Assets/Scripts/ObjectHit.cs
+++ Assets/Scripts/ObjectHit.cs
@@ -4,6 +4,7 @@ public class ObjectHit : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
+        GetComponent<MeshRenderer>().material.color = Color.red;
         Debug.Log("무언가 충돌했어요!");
     }
 }
