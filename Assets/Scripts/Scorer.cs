bb1184f1b75965839e69ed2cbb468189c40091ba
BreadOne <bbangwon.chung@gmail.com>
2026-01-18 17:36:00 +0900

    Scorer 스크립트 추가 및 패키지/에디터 버전 업데이트
    SampleScene에 Scorer 컴포넌트를 추가하고, 충돌 횟수를 기록하는 Scorer.cs 스크립트를 새로 생성했습니다. ObjectHit.cs의 디버그 메시지를 제거했으며, 일부 오브젝트 위치를 소폭 조정했습니다. Unity 패키지와 에디터 버전을 최신으로 업데이트했습니다.

---
+++ Assets/Scripts/Scorer.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Scorer : MonoBehaviour
+{
+    int hits = 0;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        hits++;
+        Debug.Log("이 물체에 이렇게 여러 번 부딪혔습니다: " + hits);
+    }
+
+}
