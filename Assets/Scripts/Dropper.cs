46b0166456c2f99f4b08b5d96d0e4b193b17f0d7
BreadOne <bbangwon.chung@gmail.com>
2026-02-20 19:58:29 +0900

    Dropper 오브젝트 중력 및 렌더링 제어 로직 추가
    Dropper.cs에서 MeshRenderer와 Rigidbody를 캐싱하여, 오브젝트가 처음에는 보이지 않고 중력의 영향을 받지 않도록 설정했습니다. 지정된 시간이 지나면 오브젝트가 보이고 중력이 적용되어 떨어지도록 구현했습니다. SampleScene.unity에서는 Dropping Object에 Rigidbody 컴포넌트를 추가하고, 초기 중력 사용을 비활성화했습니다.

--- Assets/Scripts/Dropper.cs
+++ Assets/Scripts/Dropper.cs
@@ -3,13 +3,27 @@ using UnityEngine;
 public class Dropper : MonoBehaviour
 {
     [SerializeField]
-    float timeToWait = 2f;    
-    
+    float timeToWait = 2f;
+
+    MeshRenderer meshRenderer;
+    Rigidbody rigidBody;
+
+    private void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        rigidBody = GetComponent<Rigidbody>();
+
+        meshRenderer.enabled = false;
+        rigidBody.useGravity = false;
+    }
+
     void Update()
     {
         if(Time.time > timeToWait)
         {
             Debug.Log("아래를 조심하세요!");
+            meshRenderer.enabled = true;
+            rigidBody.useGravity = true;
         }
     }
 }
