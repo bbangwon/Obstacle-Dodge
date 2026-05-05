7b3a4b6b9314781326dc6204c4c86ae9dae3b81a
BreadOne <bbangwon.chung@gmail.com>
2026-05-05 20:17:50 +0900

    발사체 트리거하기

---
+++ Assets/Scripts/TriggerProjectile.cs
@@ -0,0 +1,18 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TriggerProjectile : MonoBehaviour
+{
+    [SerializeField] GameObject[] projectiles;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            foreach (GameObject projectile in projectiles)
+            {
+                projectile.SetActive(true);
+            }
+        }
+    }
+}
